b7064428dd57d829b0aebecb2953b665a4e57fc7
sumann <sumannnarya6969@gmail.com>
2025-02-08 19:32:42 +0530

    cumit

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -18,7 +18,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        Shop the Latest Styles, Stay Forever in Fashion! 🛍️✨
         </p>
       </div>
 
--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -10,7 +10,7 @@ const NewsletterBox = () => {
   return (
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-      <p className='text-gray-400 mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p className='text-gray-400 mt-3'>Stay Ahead in Style! Subscribe to our newsletter for exclusive deals, trend alerts, and the latest drops from Forever. Don’t miss out—be the first to shop!</p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' required type="email" placeholder='Enter your email' />
         <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
